8a3eeb48292b5274d1a12f05956f339affe6f4c4
Christian Sletten <35894685+csletten@users.noreply.github.com>
2022-10-28 13:44:59 +0200

    Code refactoring to import functions using scripts Dataset loads from relative file path

---
+++ dataset.py
@@ -0,0 +1,70 @@
+import scipy
+import os
+import numpy as np
+import pandas as pd
+
+DIR_RAW = 'Data/raw_data'
+DIR_FILTERED = 'Data/filtered_data'
+
+LABELS_PATH = 'Data/scales.xls'
+
+COLUMNS_TO_RENAME = {
+    'Subject No.': 'subject_no',
+    'Trial_1': 't1_math',
+    'Unnamed: 2': 't1_symmetry',
+    'Unnamed: 3': 't1_stroop',
+    'Trial_2': 't2_math',
+    'Unnamed: 5': 't2_symmetry',
+    'Unnamed: 6': 't2_stroop',
+    'Trial_3': 't3_math',
+    'Unnamed: 8': 't3_symmetry',
+    'Unnamed: 9': 't3_stroop'
+}
+
+
+def load_dataset(raw=True, test_type="Arithmetic"):
+    '''
+    Loads data from the SAM 40 Dataset with the test specified by test_type.
+    The raw flag specifies whether to use the raw data or the filtered data.
+    Returns a Numpy Array with shape (120, 32, 3200).
+    '''
+    if raw:
+        dir = DIR_RAW
+        data_key = 'Data'
+    else:
+        dir = DIR_FILTERED
+        data_key = 'Clean_data'
+
+    dataset = np.empty((120, 32, 3200))
+
+    counter = 0
+    for filename in os.listdir(dir):
+        if test_type not in filename:
+            continue
+
+        f = os.path.join(dir, filename)
+        data = scipy.io.loadmat(f)[data_key]
+        dataset[counter] = data
+        counter += 1
+    return dataset
+
+
+def load_labels():
+    labels = pd.read_excel(LABELS_PATH)
+    labels = labels.rename(columns=COLUMNS_TO_RENAME)
+    labels = labels[1:]
+    labels = labels.astype("int")
+    labels = labels > 5
+    return labels
+
+
+def convert_to_epochs(dataset, channels, sfreq):
+    '''
+    Splits EEG data into epochs with length specified by epoch_length
+    '''
+    epoched_dataset = np.empty(
+        (dataset.shape[0], dataset.shape[2]//sfreq, channels, sfreq))
+    for i in range(dataset.shape[0]):
+        for j in range(dataset.shape[2]//sfreq):
+            epoched_dataset[i, j] = dataset[i, :, j*sfreq:(j+1)*sfreq]
+    return epoched_dataset
---
+++ features.py
@@ -0,0 +1,79 @@
+import mne
+import mne_features
+import numpy as np
+
+def time_series_features(data, channels):
+    stats_to_compute = 4
+    n_features = channels* stats_to_compute
+
+    features = np.empty([data.shape[0], data.shape[1], n_features])
+    for i, trial in enumerate(data):
+        for j, second in enumerate(trial):
+            mean = mne_features.univariate.compute_mean(second)
+            variance = mne_features.univariate.compute_variance(second)
+            skewness = mne_features.univariate.compute_skewness(second)
+            rms = mne_features.univariate.compute_rms(second)
+            stats = np.concatenate([mean, variance, skewness, rms])
+            features[i][j] = stats
+    features = features.reshape([features.shape[0]*features.shape[1], features.shape[2]])
+    return features
+
+def nonlinear_features(data, channels):
+    stats_to_compute = 3
+    n_features = channels* stats_to_compute
+
+    features = np.empty([data.shape[0], data.shape[1], n_features])
+    for i, trial in enumerate(data):
+        for j, second in enumerate(trial):
+            hurst = mne_features.univariate.compute_hurst_exp(second)
+            higuchi = mne_features.univariate.compute_higuchi_fd(second)
+            katz = mne_features.univariate.compute_katz_fd(second)
+            svd_entropy = mne_features.univariate.compute_svd_entropy(second)
+            entropies = np.concatenate([hurst, higuchi, katz])
+            features[i][j] = entropies
+    features = features.reshape([features.shape[0]*features.shape[1], features.shape[2]])
+    return features
+
+def entropy_features(data, channels, sfreq):
+    stats_to_compute = 4
+    n_features = channels* stats_to_compute
+
+    features = np.empty([data.shape[0], data.shape[1], n_features])
+    for i, trial in enumerate(data):
+        for j, second in enumerate(trial):
+            app_entropy = mne_features.univariate.compute_app_entropy(second)
+            samp_entropy = mne_features.univariate.compute_samp_entropy(second)
+            spect_entropy = mne_features.univariate.compute_spect_entropy(sfreq, second)
+            svd_entropy = mne_features.univariate.compute_svd_entropy(second)
+            entropies = np.concatenate([app_entropy, samp_entropy, spect_entropy, svd_entropy])
+            features[i][j] = entropies
+    features = features.reshape([features.shape[0]*features.shape[1], features.shape[2]])
+    return features
+
+def hjorth_features(data, channels, sfreq):
+    stats_to_compute = 4
+    n_features = channels* stats_to_compute
+
+    features = np.empty([data.shape[0], data.shape[1], n_features])
+    for i, trial in enumerate(data):
+        for j, second in enumerate(trial):
+            mobility_spect = mne_features.univariate.compute_hjorth_mobility_spect(sfreq, second)
+            complexity_spect = mne_features.univariate.compute_hjorth_complexity_spect(sfreq, second)
+            mobility = mne_features.univariate.compute_hjorth_mobility(second)
+            complexity = mne_features.univariate.compute_hjorth_complexity(second)
+            hjorth = np.concatenate([mobility_spect, complexity_spect, mobility, complexity])
+            features[i][j] = hjorth
+    features = features.reshape([features.shape[0]*features.shape[1], features.shape[2]])
+    return features
+
+def freq_band_features(data, channels, sfreq, freq_bands):
+    features = np.empty([0, channels*(len(freq_bands)-1)])
+    for trial in data:
+        PSDh2 = np.empty([0, channels*(len(freq_bands)-1)])
+        for second in trial:
+            PSD_delta = mne_features.univariate.compute_pow_freq_bands(
+                sfreq, second, freq_bands=freq_bands, normalize=True, ratios=None, ratios_triu=False, psd_method='welch', log=False, psd_params=None)
+            PSDh2 = np.vstack((PSDh2, PSD_delta))
+        features = np.vstack((features, PSDh2))
+    return features
+
