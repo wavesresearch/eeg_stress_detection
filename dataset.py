743048e4b7344cd84d164bea8e054bb167fe9a08
Christian Sletten <35894685+csletten@users.noreply.github.com>
2022-11-09 08:49:36 +0100

    Moved functions to scripts

--- dataset.py
+++ dataset.py
@@ -11,14 +11,22 @@ LABELS_PATH = 'Data/scales.xls'
 COLUMNS_TO_RENAME = {
     'Subject No.': 'subject_no',
     'Trial_1': 't1_math',
-    'Unnamed: 2': 't1_symmetry',
+    'Unnamed: 2': 't1_mirror',
     'Unnamed: 3': 't1_stroop',
     'Trial_2': 't2_math',
-    'Unnamed: 5': 't2_symmetry',
+    'Unnamed: 5': 't2_mirror',
     'Unnamed: 6': 't2_stroop',
     'Trial_3': 't3_math',
-    'Unnamed: 8': 't3_symmetry',
+    'Unnamed: 8': 't3_mirror',
     'Unnamed: 9': 't3_stroop'
+}
+
+TEST_TYPES = ["Arithmetic", "Mirror", "Stroop"]
+
+TEST_TYPE_COLUMNS = {
+    'Arithmetic': ['t1_math', 't2_math', 't3_math'],
+    'Mirror': ['t1_mirror', 't2_mirror', 't3_mirror'],
+    'Stroop': ['t1_stroop', 't2_stroop', 't3_stroop']
 }
 
 
@@ -28,6 +36,8 @@ def load_dataset(raw=True, test_type="Arithmetic"):
     The raw flag specifies whether to use the raw data or the filtered data.
     Returns a Numpy Array with shape (120, 32, 3200).
     '''
+    assert (test_type in TEST_TYPES)
+
     if raw:
         dir = DIR_RAW
         data_key = 'Data'
@@ -62,12 +72,30 @@ def load_labels():
     return labels
 
 
-def convert_to_epochs(dataset, channels, sfreq):
+def format_labels(labels, test_type="Arithmetic", epochs=1):
     '''
-    Splits EEG data into epochs with length 1 sec
+    Filter the labels to keep the labels from the test type specified by test_type.
+    Repeat the labels by the amount of epochs in a recording, specified by epochs.
+    '''
+    assert (test_type in TEST_TYPES)
+
+    formatted_labels = []
+    for trial in TEST_TYPE_COLUMNS[test_type]:
+        formatted_labels.append(labels[trial])
+
+    formatted_labels = pd.concat(formatted_labels).to_numpy()
+
+    formatted_labels = formatted_labels.repeat(epochs)
+
+    return formatted_labels
+
+
+def convert_to_epochs(dataset, n_channels, sfreq):
+    '''
+    Splits EEG data into epochs with length 1 sec.
     '''
     epoched_dataset = np.empty(
-        (dataset.shape[0], dataset.shape[2]//sfreq, channels, sfreq))
+        (dataset.shape[0], dataset.shape[2]//sfreq, n_channels, sfreq))
     for i in range(dataset.shape[0]):
         for j in range(dataset.shape[2]//sfreq):
             epoched_dataset[i, j] = dataset[i, :, j*sfreq:(j+1)*sfreq]
--- features.py
+++ features.py
@@ -1,15 +1,15 @@
 import mne_features
 import numpy as np
 
-def time_series_features(data, channels):
+
+def time_series_features(data, n_channels):
     '''
     Generate the features mean, variance, skewness and rms using the package mne_features.
     The data should be on the form (n_trials, n_secs, n_channels, sfreq)
     The output is on the form (n_trials*n_secs, n_channels*n_features)
     '''
-    stats_to_compute = 4
-    n_features = channels* stats_to_compute
-    print(data.shape)
+    features_to_compute = 4
+    n_features = n_channels * features_to_compute
 
     features = np.empty([data.shape[0], data.shape[1], n_features])
     for i, trial in enumerate(data):
@@ -18,19 +18,20 @@ def time_series_features(data, channels):
             variance = mne_features.univariate.compute_variance(second)
             skewness = mne_features.univariate.compute_skewness(second)
             rms = mne_features.univariate.compute_rms(second)
-            stats = np.concatenate([mean, variance, skewness, rms])
-            features[i][j] = stats
-    features = features.reshape([features.shape[0]*features.shape[1], features.shape[2]])
+            features[i][j] = np.concatenate([mean, variance, skewness, rms])
+    features = features.reshape(
+        [features.shape[0]*features.shape[1], features.shape[2]])
     return features
 
-def nonlinear_features(data, channels):
+
+def nonlinear_features(data, n_channels):
     '''
     Compute the features Hurst exponent, Higuchi Fractal Dimension and Katz Fractal Dimension using the package mne_features.
     The data should be on the form (n_trials, n_secs, n_channels, sfreq)
     The output is on the form (n_trials*n_secs, n_channels*n_features)
     '''
-    stats_to_compute = 3
-    n_features = channels* stats_to_compute
+    features_to_compute = 3
+    n_features = n_channels * features_to_compute
 
     features = np.empty([data.shape[0], data.shape[1], n_features])
     for i, trial in enumerate(data):
@@ -38,66 +39,77 @@ def nonlinear_features(data, channels):
             hurst = mne_features.univariate.compute_hurst_exp(second)
             higuchi = mne_features.univariate.compute_higuchi_fd(second)
             katz = mne_features.univariate.compute_katz_fd(second)
-            entropies = np.concatenate([hurst, higuchi, katz])
-            features[i][j] = entropies
-    features = features.reshape([features.shape[0]*features.shape[1], features.shape[2]])
+            features[i][j] = np.concatenate([hurst, higuchi, katz])
+    features = features.reshape(
+        [features.shape[0]*features.shape[1], features.shape[2]])
     return features
 
-def entropy_features(data, channels, sfreq):
+
+def entropy_features(data, n_channels, sfreq):
     '''
     Compute the features Approximate Entropy, Sample Entropy, Spectral Entropy and SVD entropy using the package mne_features.
     The data should be on the form (n_trials, n_secs, n_channels, sfreq)
     The output is on the form (n_trials*n_secs, n_channels*n_features)
     '''
-    stats_to_compute = 4
-    n_features = channels* stats_to_compute
+    features_to_compute = 4
+    n_features = n_channels * features_to_compute
 
     features = np.empty([data.shape[0], data.shape[1], n_features])
     for i, trial in enumerate(data):
         for j, second in enumerate(trial):
             app_entropy = mne_features.univariate.compute_app_entropy(second)
             samp_entropy = mne_features.univariate.compute_samp_entropy(second)
-            spect_entropy = mne_features.univariate.compute_spect_entropy(sfreq, second)
+            spect_entropy = mne_features.univariate.compute_spect_entropy(
+                sfreq, second)
             svd_entropy = mne_features.univariate.compute_svd_entropy(second)
-            entropies = np.concatenate([app_entropy, samp_entropy, spect_entropy, svd_entropy])
-            features[i][j] = entropies
-    features = features.reshape([features.shape[0]*features.shape[1], features.shape[2]])
+            features[i][j] = np.concatenate(
+                [app_entropy, samp_entropy, spect_entropy, svd_entropy])
+    features = features.reshape(
+        [features.shape[0]*features.shape[1], features.shape[2]])
     return features
 
-def hjorth_features(data, channels, sfreq):
+
+def hjorth_features(data, n_channels, sfreq):
     '''
     Compute the features Hjorth mobility (spectral), Hjorth complexity (spectral), Hjorth mobility and Hjorth complexity using the package mne_features.
     The data should be on the form (n_trials, n_secs, n_channels, sfreq)
     The output is on the form (n_trials*n_secs, n_channels*n_features)
     '''
-    stats_to_compute = 4
-    n_features = channels* stats_to_compute
+    features_to_compute = 4
+    n_features = n_channels * features_to_compute
 
     features = np.empty([data.shape[0], data.shape[1], n_features])
     for i, trial in enumerate(data):
         for j, second in enumerate(trial):
-            mobility_spect = mne_features.univariate.compute_hjorth_mobility_spect(sfreq, second)
-            complexity_spect = mne_features.univariate.compute_hjorth_complexity_spect(sfreq, second)
+            mobility_spect = mne_features.univariate.compute_hjorth_mobility_spect(
+                sfreq, second)
+            complexity_spect = mne_features.univariate.compute_hjorth_complexity_spect(
+                sfreq, second)
             mobility = mne_features.univariate.compute_hjorth_mobility(second)
-            complexity = mne_features.univariate.compute_hjorth_complexity(second)
-            hjorth = np.concatenate([mobility_spect, complexity_spect, mobility, complexity])
-            features[i][j] = hjorth
-    features = features.reshape([features.shape[0]*features.shape[1], features.shape[2]])
+            complexity = mne_features.univariate.compute_hjorth_complexity(
+                second)
+            features[i][j] = np.concatenate(
+                [mobility_spect, complexity_spect, mobility, complexity])
+    features = features.reshape(
+        [features.shape[0]*features.shape[1], features.shape[2]])
     return features
 
-def freq_band_features(data, channels, sfreq, freq_bands):
+
+def freq_band_features(data, n_channels, sfreq, freq_bands):
     '''
     Compute the frequency bands delta, theta, alpha, beta and gamma using the package mne_features.
     The data should be on the form (n_trials, n_secs, n_channels, sfreq)
     The output is on the form (n_trials*n_secs, n_channels*n_features)
     '''
-    features = np.empty([0, channels*(len(freq_bands)-1)])
-    for trial in data:
-        PSDh2 = np.empty([0, channels*(len(freq_bands)-1)])
-        for second in trial:
-            PSD_delta = mne_features.univariate.compute_pow_freq_bands(
-                sfreq, second, freq_bands=freq_bands, normalize=True, ratios=None, ratios_triu=False, psd_method='welch', log=False, psd_params=None)
-            PSDh2 = np.vstack((PSDh2, PSD_delta))
-        features = np.vstack((features, PSDh2))
-    return features
+    features_to_compute = len(freq_bands)-1
+    n_features = n_channels*features_to_compute
 
+    features = np.empty([data.shape[0], data.shape[1], n_features])
+    for i, trial in enumerate(data):
+        for j, second in enumerate(trial):
+            PSD = mne_features.univariate.compute_pow_freq_bands(
+                sfreq, second, freq_bands=freq_bands, normalize=True, ratios=None, ratios_triu=False, psd_method='welch', log=False, psd_params=None)
+            features[i][j] = PSD
+    features = features.reshape(
+        [features.shape[0]*features.shape[1], features.shape[2]])
+    return features
